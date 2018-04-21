ce3bc424f0258fda7c0f868bb02207ff9103c226
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-21 11:06:30 -0400

    HTML progress

---
+++ timesheet.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBxrYsqfT2iSjKjuWWL_KdetZ73B_jG6Bw",
+    authDomain: "employeetimesheet-802da.firebaseapp.com",
+    databaseURL: "https://employeetimesheet-802da.firebaseio.com",
+    projectId: "employeetimesheet-802da",
+    storageBucket: "employeetimesheet-802da.appspot.com",
+    messagingSenderId: "840254052705"
+  };
+  firebase.initializeApp(config);
