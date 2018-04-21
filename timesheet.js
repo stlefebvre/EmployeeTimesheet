364cf37755b50d789e08c9f1f3079866d7c815fd
JB <jmb2883@gmail.com>
2018-04-21 10:49:03 -0400

    adding database info to js file

--- timesheet.js
+++ timesheet.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
  
-var employeeInfo = [];
+  var config = {
+    apiKey: "AIzaSyBxrYsqfT2iSjKjuWWL_KdetZ73B_jG6Bw",
+    authDomain: "employeetimesheet-802da.firebaseapp.com",
+    databaseURL: "https://employeetimesheet-802da.firebaseio.com",
+    projectId: "employeetimesheet-802da",
+    storageBucket: "employeetimesheet-802da.appspot.com",
+    messagingSenderId: "840254052705"
+  };
+  firebase.initializeApp(config);
+
+
+1 Comment
+
+
+  var employeeInfo = [];
 
  $("body").on("click", function addRow() {
   $("#resultsContainer").empty();
